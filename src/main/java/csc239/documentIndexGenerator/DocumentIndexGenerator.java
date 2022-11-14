cf4b4d82563575d7511f9ff18b106a1f6e0a17d4
Margo <kholostova@gmail.com>
2022-11-14 12:36:56 -0500

    initialized classes

---
+++ src/main/java/csc239/documentIndexGenerator/DocumentIndexGenerator.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package csc239.documentIndexGenerator;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author admin
+ */
+public class DocumentIndexGenerator {
+
+    private static String fileName;
+    private static File docFile;
+    private static ArrayList<LowercaseWord> wordList;
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/csc239/documentIndexGenerator/LowercaseWord.java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author admin
+ */
+class LowercaseWord implements Comparable<LowercaseWord> {
+
+    private String lowerCaseText;
+    private int occurenceCount = 1;
+    private ArrayList<Occurence> refList;
+
+    public int compareTo(LowercaseWord w) {
+        return 0;
+    }
+}
