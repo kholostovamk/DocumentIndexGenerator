3e74ebde026354bb319b23f0a08c0eb78ada1650
Margo <kholostova@gmail.com>
2022-11-28 14:17:52 -0500

    moved a file, changed Read File method, does not compile

--- src/main/java/csc239/documentIndexGenerator/LowercaseWord.java
+++ src/main/java/csc239/documentIndexGenerator/LowercaseWord.java
@@ -2,6 +2,7 @@ package csc239.documentIndexGenerator;
 
 
 import java.util.ArrayList;
+import java.util.List;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -11,13 +12,47 @@ import java.util.ArrayList;
  *
  * @author admin
  */
-class LowercaseWord implements Comparable<LowercaseWord> {
+class LowercaseWord  {
 
     private String lowerCaseText;
     private int occurenceCount = 1;
     private ArrayList<Occurence> refList;
-
-    public int compareTo(LowercaseWord w) {
-        return 0;
+    
+  // private String word;
+  //private int noOfOccurences;
+  //private List<Occurence> occurences;
+  public LowercaseWord(String lowerCaseText) {
+    this.lowerCaseText = lowerCaseText;
+    this.occurenceCount = 1;
+    refList = new ArrayList<>();
+  }
+  // adding occurence of a word
+  public void addOccurence(int lineNum, int wordNum) {
+    Occurence occurence = new Occurence(lineNum,wordNum);
+    refList.add(occurence);
+    occurenceCount++;
+  }
+  
+  public List<Occurence> getOccurences() {
+    return refList;
+  }
+  
+  public String getWord() { return lowerCaseText; }
+  
+  
+  @Override
+  public String toString() {
+    String string = lowerCaseText + "(" + occurenceCount + "): [";
+    int count = 0;
+    for (Occurence occurence : refList) {
+      string += "(" + occurence.getLineNum() + ", " + occurence.getWordNum() + ")";
+      count++;
+      if(count == occurenceCount) {
+      } else {
+          string += ",";
+        }
     }
-}
+    string += "]";
+    return string;
+  }
+
