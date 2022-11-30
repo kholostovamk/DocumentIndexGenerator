cb44df6bee290ec964b5c86289e1355017cb6fd2
Margo <kholostova@gmail.com>
2022-11-30 14:30:08 -0500

    throws compiling error "can't use iterator"

--- src/main/java/csc239/documentIndexGenerator/DocumentIndexGenerator.java
+++ src/main/java/csc239/documentIndexGenerator/DocumentIndexGenerator.java
@@ -10,7 +10,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -18,35 +18,52 @@ import java.util.Scanner;
  *
  * @author admin
  */
+
+
+class Occurence {
+ 
+    private int lineNum, wordNum;
+    
+  public Occurence(int lineNum, int wordNum) {
+    this.lineNum = lineNum;
+    this.wordNum = wordNum;
+  }
+  public int getLineNum() { return lineNum; }
+  public int getWordNum() { return wordNum; }
+}
+
+
+
 public class DocumentIndexGenerator {
 
     private static String fileName;
     private static File docFile;
-    
     private static ArrayList<LowercaseWord> wordList;
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
 
-    String line;
-    int lineNum = 0;
-    int wordNum = 0;
+ 
   
-    String[] words = null;      
+    String[] words = {};      
     //reads file
     System.out.println("Enter fileName: ");
 
     Scanner input = new Scanner(System.in);
     fileName = input.nextLine().trim(); 
     
-    System.out.println(fileName); // - works
-    
-    String text = readString(fileName);
-    System.out.println(text);
-    
-    String[] arrOfStrings = text.toLowerCase().split(" ");
+   // System.out.println(fileName); // - works
    
-    for (String a : arrOfStrings)
-            System.out.println(a);
+   docFile = new File(fileName);
+    
+    readString(fileName, words);
+    
+   // String text = readString(fileName);
+    //System.out.println(text);
+   // System.out.println(lineNum);
+    
+   // String[] words = text.toLowerCase().split(" ");
+   
+
     
 
     
@@ -59,16 +76,74 @@ public class DocumentIndexGenerator {
   //built always sorted list? compare with item in the list
 
     }
+    
+    
+    public static LowercaseWord getWordFromList(String word) {
+    for(LowercaseWord lowerWord : wordList) {
+      if(lowerWord.getWord().equalsIgnoreCase(word)) {
+        return lowerWord;
+      }
+    }
+   // return null;
+  }
+
    
-    public static String readString(String file) throws FileNotFoundException{
-        docFile = new File(fileName);
+    public static void readString(String file, String[] words ) throws FileNotFoundException, IOException{
         
+       
+      //  FileInputStream fileInputStream = new FileInputStream(fileName);
+       // InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+  
+        String line;
+        int lineNum = 0;
+        
+        
+       
+        while ((line = bufferedReader.readLine()) != null) {
+           
+         
+                words = line.split(" ");
+                lineNum++;
+                int wordNum = 0;
+                for (String word: words) {
+                    wordNum++;
+                    word = word.toLowerCase();
+                    
+                    LowercaseWord lowerWord= getWordFromList(word);
+                    
+                    if (lowerWord == null) {
+                        lowerWord = new LowercaseWord (word);
+                        wordList.add(lowerWord);
+                    } 
+                    
+                    lowerWord.addOccurence(lineNum, wordNum);
+                    System.out.println(lowerWord);
+          
+                
+                }
+        }
+        bufferedReader.close();
+        
+    }
+}
+       
+       
+      
+
+
+
+
+/*public static String readString(String file, String[] words) throws FileNotFoundException{
+        docFile = new File(fileName);
+       
         String text = " ";
         
         try {
             Scanner s = new Scanner(docFile);
-            while(s.hasNext()){
-                text = text + s.next()+ " ";
+            while(s.hasNextLine()){
+                text = text + s.nextLine();
+                lineNum++;
             }
         
         }
@@ -79,6 +154,4 @@ public class DocumentIndexGenerator {
         
         return text;
 }
-}
-
-
+}*/
